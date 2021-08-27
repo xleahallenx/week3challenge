71cd1c8c8c4e61c80f271aa61eb076d0efe1a745
Leah <leah@Jamess-MBP.attlocal.net>
2021-08-26 23:20:10 -0500

    if/for

--- script.js
+++ script.js
@@ -12,22 +12,46 @@
   // set textArea value to be random password
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-var possibleCharacters = ""
-var characters = "qwertyuiopasdfghjklzxcvbnm"
-var lowerConfirm = confirm("do you like puppies")
-var upperConfirm = 
-var specialConfirm =
-var integersConfirm =
-if(confirmThis == true){
-  possibleCharacters += characters
+const generateBtn = document.querySelector("#generate");
+let possibleCharacters = ""
+const lowerCharacters = "qwertyuiopasdfghjklzxcvbnm"
+const upperCharacters = "ASDFGHJKLQWERTYUIOPZXCVBNM"
+const specialCharacters = "!@#$%^&*"
+const integers = "1234567890"
+const lowerConfirm = confirm("Would you like to use lower case letters in your password?")
+const upperConfirm = confirm("Would you like to use upper case letters in your password?")
+const specialConfirm = confirm("Would you like to use symbols in your password?")
+const integersConfirm = confirm("Would you like to use numbers in your password?")
+
+
+if(lowerConfirm == true){
+  possibleCharacters += lowerCharacters 
+}
+if(upperConfirm == true) {
+possibleCharacters += upperCharacters
+}
+  
+if(specialConfirm == true) {
+  possibleCharacters += specialCharacters
+}
+if(integersConfirm == true) {
+  possibleCharacters += integers
 }
 // 
-// possibleCharacters += characters.toUpperCase()
-console.log(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)])
+// possibleCharacters += lowerCharacters, upperCharacters, specialCharacters, integers()
+//console.log(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)])
+//const generatePassword = (length, possibleCharacters)
+  //let password = "";
+  //for(let i = 0, i < length; i++) {
+    //password += characters.charAt(
+      //Math.floor(Math.random() * characters.length)
+    //);
+  //}
+
+
 function generatePassword () {
 
-  return "this is the string"
+  return password
 }
 
 
